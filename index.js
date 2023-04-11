b3fd4e433783a3d2c212ef4262b80b23c79788bb
shikhaarya170 <shikhaarya170@gmail.com>
2023-04-11 17:21:24 +0530

    message

---
+++ index.js
@@ -0,0 +1,87 @@
+
+
+
+// document.querySelector('button').addEventListener('click',function() {
+//     alert('I got clicked');
+// });
+
+
+// document.querySelector('button').addEventListener('click' ,handleclick);
+
+
+// function handleclick(){
+//     alert('I  clicked');
+// }
+
+  var NumberOfDrumButtons = document.querySelectorAll(
+    '.drum').length;
+
+for(let i=0;i<NumberOfDrumButtons;i++){
+document.querySelectorAll(' .drum')[i].addEventListener('click',function() {
+    alert('i got clicked')
+});
+}
+
+
+//through indexing 
+// document.querySelectorAll('button')[0].addEventListener('click',function() {
+//     alert('i got clicked')
+// });
+
+// document.querySelectorAll('button')[1].addEventListener('click',function() {
+//     alert('i got clicked')
+// });
+// document.querySelectorAll('button')[2].addEventListener('click',function() {
+//     alert('i got clicked')
+// });
+// document.querySelectorAll('button')[3].addEventListener('click',function() {
+//     alert('i got clicked')
+// });
+
+
+// document.querySelectorAll('button')[4].addEventListener('click',function() {
+//     alert('i got clicked')
+// });
+
+
+// document.querySelectorAll('button')[5].addEventListener('click',function() {
+//     alert('i got clicked')
+// });
+
+
+
+
+// document.querySelectorAll('button')[6].addEventListener('click',function() {
+//     alert('i got clicked')
+// });
+
+
+//through class
+// document.querySelector('.w.drum').addEventListener('click',function(){
+//     alert('i got clicked')
+// });
+
+// document.querySelector('.a.drum').addEventListener('click',function(){
+//     alert('i got clicked')
+// });
+
+// document.querySelector('.s.drum').addEventListener('click',function(){
+//     alert('i got clicked')
+// });
+
+// document.querySelector('.d.drum').addEventListener('click',function(){
+//     alert('i got clicked')
+// });
+
+// document.querySelector('.j.drum').addEventListener('click',function(){
+//     alert('i got clicked')
+// });
+
+// document.querySelector('.k.drum').addEventListener('click',function(){
+//     alert('i got clicked')
+// });
+
+// document.querySelector('.l.drum').addEventListener('click',function(){
+//     alert('i got clicked')
+// });
+
